a66615adf98850a3e0491253ac7f4491ec63db0d
shreyas66s <110161231+shreyas66s@users.noreply.github.com>
2024-07-26 11:03:58 +0530

    Update consts.go

--- consts/consts.go
+++ consts/consts.go
@@ -13,11 +13,11 @@ import (
 
 const (
 	// TODO: choose a human-readable part for your hyperchain
-	HRP = ""
+	HRP = "shreyas"
 	// TODO: choose a name for your hyperchain
-	Name = ""
+	Name = "jaguar"
 	// TODO: choose a token symbol
-	Symbol = ""
+	Symbol = "JAGU"
 )
 
 var ID ids.ID
